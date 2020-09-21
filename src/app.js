cfbc2a66b9e10a2746f10703c5d8dc4c7b75371d
Theerasak Buakai <theerasak.bu@inet.co.th>
2020-09-22 01:32:36 +0700

    Add comment

--- src/app.js
+++ src/app.js
@@ -3,4 +3,5 @@ import ReactDOM from 'react-dom'
 import IndecisionApp from "./component/IndecicionApp.js";
 import 'normalize.css/normalize.css'
 import '../src/styles/styles.scss'
+//
 ReactDOM.render(<p>This is my boilerplate</p>, document.getElementById("app"))
